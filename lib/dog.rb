322140678bce82a566856d4d77791b5610a88055
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-15 23:37:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+  #setter
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  #getter
+  def name
+    @this_dogs_name
+  end
+
+  #bark
+  def bark
+    puts "woof!"
+  end
+end
